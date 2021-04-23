3532adad29b90e673482fe2d8378058f814147d6
216205999 <216205999@mycput.ac.za>
2021-04-23 14:34:11 +0200

    finally version

---
+++ src/main/java/za/ac/cput/product.java
@@ -0,0 +1,33 @@
+package za.ac.cput;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Disabled;
+
+public class product {
+
+    public AddittionIT() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+
+}
---
+++ src/test/java/za/ac/cput/productTest.java
@@ -0,0 +1,43 @@
+package za.ac.cput;
+
+
+
+
+public class productTest {
+
+    @Test
+    public void testSum() {
+        System.out.println("sum");
+        double number1 = 0.0;
+        double number2 = 0.0;
+        double expResult = 0.0;
+        double result = Addittion.sum(number1, number2);
+        assertEquals(expResult, result, 0.0);
+
+    }
+    @Test
+    public void objectEquality(double number1,double number2){
+        assertEquals(number1,number2) ;
+    }
+    @Test
+    public void testIdentity(double number1,double number2){
+        assertSame(number1,number2);
+    }
+    @Test
+    public void TestFailling(){
+        fail("failed");
+    }
+
+    @Test
+    @Disabled()
+    public void disablingTest(){
+
+    }
+
+    @Test
+    public void timeOutExceededTest() {
+
+
+
+    }
+}
